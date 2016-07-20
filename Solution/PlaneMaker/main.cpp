d32aed4cea9c84a2fed9be338311fe462f94a325
Sunshine <sunshine233333@gmail.com>
2016-07-20 14:03:25 +0900

    チケット番号＃1214 ダミーモデルの制作

---
+++ Solution/PlaneMaker/main.cpp
@@ -0,0 +1,46 @@
+#include "Framework.h"
+#include "Model.h"
+#include "DxLib.h"
+
+const double CHIP_SIZE = 0.5;
+const double TEXTURE_SIZE = 0.5;
+const double T = TEXTURE_SIZE / 2;
+
+enum COLOR_TYPE {
+	COLOR_TYPE_RED,
+	COLOR_TYPE_BLUE,
+	COLOR_TYPE_MAX
+};
+
+const std::string MODEL_NAME[ COLOR_TYPE_MAX ] = {
+	"Pillar",
+	"Plain",
+};
+
+void makePlane ( int type ) {
+	ModelPtr model = ModelPtr( new Model( ) );
+	int polygon_num = 2;
+	model->alloc( polygon_num );
+
+	int idx = 0;
+
+	double tex_pos_x = type * TEXTURE_SIZE;
+	double tex_pos_y = 0;
+
+	model->set( idx++, Model::VERTEX( Vector( -CHIP_SIZE, -CHIP_SIZE, 0 ), tex_pos_x, tex_pos_y ) );
+	model->set( idx++, Model::VERTEX( Vector(  0, -CHIP_SIZE, 0 ), T + tex_pos_x, tex_pos_y ) );
+	model->set( idx++, Model::VERTEX( Vector(  0,  0, 0 ), T + tex_pos_x, T + tex_pos_y ) );
+	model->set( idx++, Model::VERTEX( Vector( -CHIP_SIZE, -CHIP_SIZE, 0 ), tex_pos_x, tex_pos_y ) );
+	model->set( idx++, Model::VERTEX( Vector(  0,  0, 0 ), T + tex_pos_x, T + tex_pos_y ) );
+	model->set( idx++, Model::VERTEX( Vector( -CHIP_SIZE,  0, 0 ), tex_pos_x, T + tex_pos_y ) );
+	std::string filename = MODEL_NAME[ type ] + ".mdl";
+	model->save( filename );
+}
+
+void main( ) {
+	for ( int i = 0; i < COLOR_TYPE_MAX; i++ ) {
+		makePlane( i );
+	}
+	FrameworkPtr fw = Framework::getInstance( );
+	fw->terminate( );
+}

cca752846a54605f671455bdfd2a85688486c32a
masato <masto>
2016-07-22 11:42:01 +0900

    main.cppの更新

--- Solution/Interface/main.cpp
+++ Solution/Interface/main.cpp
@@ -1,11 +1,14 @@
 #include "Framework.h"
 #include "Viewer.h"
+#include "Drawer.h"
 #include "App.h"
 
 void main( ) {
 	FrameworkPtr fw = Framework::getInstance ( );
 	ViewerPtr viewer = ViewerPtr( new Viewer );
 	AppPtr app = AppPtr( new App );
+	DrawerPtr drawer = DrawerPtr( new Drawer( "../Resource" ) );
 	fw->addTask( Viewer::getTag( ), viewer );
 	fw->addTask( App::getTag( ), app );
+	fw->addTask( Drawer::getTag( ), drawer );
 }

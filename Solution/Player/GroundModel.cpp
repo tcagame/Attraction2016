c2ba2f7b8a3e69f870bbd38c4b2fc824de6e2bab
sunshine <sunshine233333@gmail.com>
2016-08-28 12:38:26 +0900

    機能 #1365 Groundシステムの改善

--- Solution/Player/GroundModel.cpp
+++ Solution/Player/GroundModel.cpp
@@ -11,6 +11,7 @@ polygon_num( 0 ) {
 }
 
 GroundModel::GroundModel() {
+	_model_max_idx  = 0;
 	_map_floor01_filepath = "../Resource/map_model/floor01.mdl";
 	_map_path01_filepath = "../Resource/map_model/path01.mdl";
 	_map_path02_filepath = "../Resource/map_model/path02.mdl";
@@ -38,29 +39,20 @@ void GroundModel::loadModelData( ) {
 			
 			switch( type ) {
 			case GROUND_TYPE_FLOOR_01:
-				//ここでエネミーデータ読み込みをする
-				enemy_filepath = "../Resource/enemy/floor01.ene";
-				Cohort->loadBlockEnemyData( enemy_filepath );
 				model->load( _map_floor01_filepath );
-				loadModelPos( idx, i, j, model );
+				loadModelPos( i, j, model );
 				break;
 			case GROUND_TYPE_PATH_01:
-				enemy_filepath = "../Resource/enemy/path01.ene";
-				Cohort->loadBlockEnemyData( enemy_filepath );
 				model->load( _map_path01_filepath );
-				loadModelPos( idx, i, j, model );
+				loadModelPos( i, j, model );
 				break;
 			case GROUND_TYPE_PATH_02:
-				enemy_filepath = "../Resource/enemy/path02.ene";
-				Cohort->loadBlockEnemyData( enemy_filepath );
 				model->load( _map_path02_filepath );
-				loadModelPos( idx, i, j, model );
+				loadModelPos( i, j, model );
 				break;
 			case GROUND_TYPE_PATH_03:
-				enemy_filepath = "../Resource/enemy/path03.ene";
-				Cohort->loadBlockEnemyData( enemy_filepath );
 				model->load( _map_path03_filepath );
-				loadModelPos( idx, i, j, model );
+				loadModelPos( i, j, model );
 				break;
 			default:
 				break;
@@ -69,72 +61,67 @@ void GroundModel::loadModelData( ) {
 	}
 }
 
-void GroundModel::loadModelPos( int idx, int x, int y, ModelPtr model ) {
+void GroundModel::loadModelPos( int x, int y, ModelPtr model ) {
 	int polygon_num = model->getPolygonNum( );
-	_model_data[ idx ].polygon_num = polygon_num;
+	_model_data[ _model_max_idx ].polygon_num = polygon_num;
 	model->translate( Vector( x * Ground::CHIP_WIDTH, y * Ground::CHIP_HEIGHT ) );
 	int num = 0;
+	double max_x = 0;
+	double max_y = 0;
+	double min_x = 10000;
+	double min_y = 10000;
 	for ( int i = 0; i < polygon_num * 3; i++ ) {
 		Vector pos = model->getPoint( i );
-		_model_data[ idx ].pos[ num ] = pos;
+		_model_data[ _model_max_idx ].pos[ num ] = pos;
+		if ( pos.x > max_x ) {
+			max_x = pos.x;
+		}
+		if ( pos.y > max_y ) {
+			max_y = pos.y;
+		}
+		if ( pos.x < min_x ) {
+			min_x = pos.x;
+		}
+		if ( pos.y < min_y ) {
+			min_y = pos.y;
+		}
 		num++;
 	}
+	_model_data[ _model_max_idx ].max_x = max_x;
+	_model_data[ _model_max_idx ].max_y = max_y;
+	_model_data[ _model_max_idx ].min_x = min_x;
+	_model_data[ _model_max_idx ].min_y = min_y;
+
+	_model_max_idx ++;
 }
 
 bool GroundModel::isCollisionGround( Vector pos ) {
 	AppPtr app = App::getTask( );
 	GroundPtr ground = app->getGround( );
-
-	
-	double size = ( ( Ground::CHIP_WIDTH + 5) / 2.0f );
-	int x = abs( pos.x ) / size;
-	int y = abs( pos.y ) / size;
-	if ( x < 0 ) {
-		x = 0;
-	}
-	if ( y < 0 ) {
-		y = 0;
-	}
-	double chip_pos_x = pos.x - ( size * x );
-	double chip_pos_y = pos.y - ( size * y );
-
-
-	int model_idx = ground->getIdx( x, y );
-	
-	Vector pos_a = Vector( pos.x, pos.y, 100 );
-	Vector pos_b = Vector( pos.x, pos.y, -100 );
 	bool ret = false;
-	for ( int i = 0; i < 2; i++ ) {
-		
-		int add_y = 0;
-		if ( chip_pos_y > 0 ) {
-			add_y = 1;
-		} else if ( chip_pos_y < 0 ) {
-			add_y = -1;
-		}
-		if ( chip_pos_x > 2.5 ) {
-			x += 1;
-		}
-		if ( chip_pos_x < 2.5 && chip_pos_x > -2.5 ) {
-			add_y *= i;
-		}
-		if ( chip_pos_x < -2.5 ) {
-			x -= 1;
-		}
-		y += add_y;
-		if ( x < 0 ) {
-			x = 0;
-		}
-		if ( y < 0 ) {
-			y = 0;
-		}
-		ret = isCollisionModel( model_idx, pos_a, pos_b );
-		model_idx = ground->getIdx( x, y );
+	for ( int i = 0; i < _model_max_idx; i++ ) {
+		if ( _model_data[ i ].max_x < pos.x ) {
+			continue;
+		}  
+		if ( _model_data[ i ].max_y < pos.y ) {
+			continue;
+		}  
+		if ( _model_data[ i ].min_x > pos.x ) {
+			continue;
+		}  
+		if ( _model_data[ i ].min_y > pos.y ) {
+			continue;
+		}  
+		Vector pos_a = pos;
+		Vector pos_b = pos;
+		pos_a.z = 100;
+		pos_b.z = -100;
+		ret = isCollisionModel( i, pos_a, pos_b );
 		if ( ret ) {
-			return ret;
+			return true;
 		}
 	}
-	return ret;
+	return false;
 }
 
 

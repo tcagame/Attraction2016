98288edf1358decad689114a391a5ff8e8c5f5d4
金原正和 <ebipilaf000@gmail.com>
2016-09-02 00:30:25 +0900

    LiveCamera

--- Solution/Client/LiveCamera.cpp
+++ Solution/Client/LiveCamera.cpp
@@ -3,27 +3,12 @@
 const Vector START_CAMERA_POS = Vector( 30, 30, 7 );
 const Vector START_TARGET_POS = Vector( 0, 0, 0 );
 
-LiveCamera::LiveCamera( ) :
-_pos( START_CAMERA_POS ), 
-_target( START_TARGET_POS ) {
+LiveCamera::LiveCamera( ) {
 }
 
 LiveCamera::~LiveCamera( ) {
 }
 
-Vector LiveCamera::getPos( ) const {
-	return _pos;
-}
-
-Vector LiveCamera::getTarget( ) const {
-	return _target;
-}
-
-void LiveCamera::setTarget( Vector target ) {
-	_target = target;
-}
 
 void LiveCamera::update( ) {
-	Vector vec = _pos - _target;
-	_pos = _target + vec;
 }
--- Solution/Client/LiveCamera.h
+++ Solution/Client/LiveCamera.h
@@ -1,20 +1,14 @@
 #pragma once
-
+#include "Camera.h"
 #include "smart_ptr.h"
 #include "mathmatics.h"
 
 PTR( LiveCamera );
 
-class LiveCamera {
+class LiveCamera : Camera {
 public:
 	LiveCamera( );
 	virtual ~LiveCamera( );
 public:
-	Vector getPos( ) const;
-	Vector getTarget( ) const;
-	void setTarget( Vector target );
 	void update( );
-private:
-	Vector _pos;
-	Vector _target;
 };

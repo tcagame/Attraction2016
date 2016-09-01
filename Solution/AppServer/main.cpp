3fc0cdb7d87b8e70464859e532556cc75652aee0
okuty <okuty.phone@gmail.com>
2016-09-01 10:17:26 +0900

    AppServerからViewerクラスを削除

--- Solution/AppServer/main.cpp
+++ Solution/AppServer/main.cpp
@@ -1,18 +1,14 @@
 #include "AppServer.h"
-#include "Viewer.h"
 #include "Drawer.h"
 #include "Keyboard.h"
 #include "Framework.h"
 
-
 bool main( ) {
 	FrameworkPtr fw = Framework::getInstance ( );
 	AppServerPtr app_server = AppServerPtr( new AppServer );
-	ViewerPtr viewer = ViewerPtr( new Viewer );
 	DrawerPtr drawer = DrawerPtr( new Drawer( "../Resource" ) );
 	KeyboardPtr keyboard = KeyboardPtr( new Keyboard );
 
-	fw->addTask( Viewer::getTag( ), viewer );
 	fw->addTask( Drawer::getTag( ), drawer );
 	fw->addTask( AppServer::getTag( ), app_server );
 	fw->addTask( Keyboard::getTag( ), keyboard );
